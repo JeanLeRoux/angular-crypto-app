7c322749fa856585c1e41f083dcaef47fcdb2876
JeanLeRoux <jeanlr14@gmail.com>
2021-09-19 13:55:55 +0200

    init push

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class NewsService {
   constructor(
-    // Angular Modules
     private http: HttpClient
   ) {}
   getCryptoMeta() {
